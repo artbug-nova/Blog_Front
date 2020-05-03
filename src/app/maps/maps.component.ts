ddd52b441f0a2f2e1826725848e4529eb1702cad
bishop777-sys <bishop_777@rambler.ru>
2020-05-03 11:23:49 +0200

    add gmaps

---
+++ src/app/maps/maps.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-maps',
+  templateUrl: './maps.component.html',
+  styleUrls: ['./maps.component.css']
+})
+export class MapsComponent implements OnInit {
+
+  constructor() { }
+  options: any;
+
+  overlays: any[];
+
+  ngOnInit() {
+      this.options = {
+          center: {lat: 36.890257, lng: 30.707417},
+          zoom: 12
+      };
+  }
+
+}
